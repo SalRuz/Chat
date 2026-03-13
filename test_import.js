106d14b74c7854ece49aaec49ca6f6af49af8504
SalRuz <ruzelsalmanov1@gmail.com>
2026-03-13 17:10:29 +0500

    Delete test_import.js

--- test_import.js
+++
@@ -1,4 +0,0 @@
-const pv = require('prismarine-viewer');
-console.log('Экспорты prismarine-viewer:', Object.keys(pv));
-console.log('headless:', typeof pv.headless);
-console.log('mineflayer:', typeof pv.mineflayer);
